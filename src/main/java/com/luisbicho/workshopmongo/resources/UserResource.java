baa95e67fb0ee69eb390ed7b2c73be825226515d
Luis Bicho <luisbicho@proton.me>
2024-03-07 17:09:40 +0100

    UserDTO

--- src/main/java/com/luisbicho/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luisbicho/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.luisbicho.workshopmongo.resources;
 
 import com.luisbicho.workshopmongo.domain.User;
+import com.luisbicho.workshopmongo.dto.UserDTO;
 import com.luisbicho.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -21,9 +23,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User>list=userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO>listDto=list.stream().map(x->new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
