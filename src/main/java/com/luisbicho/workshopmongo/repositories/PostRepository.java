670dd1d7bd0c9c998072dfeafaf506cb9dd987c1
Luis Bicho <luisbicho@proton.me>
2024-03-11 20:31:40 +0100

    @Query

--- src/main/java/com/luisbicho/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/luisbicho/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.luisbicho.workshopmongo.repositories;
 import com.luisbicho.workshopmongo.domain.Post;
 import com.luisbicho.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,11 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{'title':  {$regex:  ?0, $options:  'i'}}")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+
 
 }
